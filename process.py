cab05e862573ef0e5acd9d2bed7ea3d92a0e6bd6
1999shaswat <1999shaswat@gmail.com>
2020-08-10 17:40:22 +0530

    Colour choice remaining,Everything else done

---
+++ process.py
@@ -0,0 +1,68 @@
+
+class Solve:
+    def __init__(self, path):
+        self.INF = 10**9
+        self.row = 0
+        self.col = 0
+        self.tot_v = 0
+        self.parent = []
+        self.visited = []
+        self.graph = path[:]
+        # self.sol()
+
+    def print_mat(self):
+        for row in self.graph:
+            print(row)
+
+    def dfs(self, edge, v):
+        self.visited[v] = 1
+        for i in range(self.tot_v):
+            if edge[v][i] > 0:
+                if self.visited[i] == 0:
+                    self.parent[i] = v
+                    self.dfs(edge, i)
+
+    def dfs_driver(self, edge, s, t):
+        self.visited=[0 for _ in range(self.tot_v)]
+        self.parent=[-1 for _ in range(self.tot_v)]
+        # for i in range(self.tot_v):
+        #     self.visited[i] = 0
+        #     self.parent[i] = -1
+        self.dfs(edge, s)
+        return self.visited[t] == 1
+
+    def ford(self, s, t):
+        # print(s,t)
+        rGraph = self.graph[:]
+        max_flow = 0
+        # self.parent = [-1 for _ in range(self.tot_v)]
+        while self.dfs_driver(rGraph, s, t):
+            path_flow = self.INF
+            v=t
+            while v!=s:
+                u=self.parent[v]
+                path_flow=min(path_flow,rGraph[u][v])
+                v=self.parent[v]
+
+            v=t
+            while v!=s:
+                u=self.parent[v]
+                rGraph[u][v]-=path_flow
+                rGraph[v][u]+=path_flow
+                v=self.parent[v]
+            max_flow+=path_flow
+        return max_flow
+            
+
+    def sol(self,s,t):
+        self.row = len(self.graph)
+        self.col = len(self.graph[0])
+        self.tot_v = self.row
+        # self.print_mat()
+        return self.ford(s,t)
+
+
+def solution(path,s,t):
+    soln = Solve(path)
+    max_flow=soln.sol(s,t)
+    return max_flow
--- sketch.py
+++ sketch.py
@@ -1,20 +1,25 @@
+from process import *
+
 from p5 import *
+from easygui import enterbox
 
 width = 800
 height = 600
 mode = 'p'
 vertex = []
 edge = None
-# mouse_lock=0
-
+msg='Vertices'
+src = None
+sink = None
+dic={}
+max_flow=0
 
 class Spot:
     def __init__(self, i, j, index):
         self.i = i
         self.j = j
         self.index = index
-        self.col = Color(random_uniform(50, 150), random_uniform(
-            50, 150), random_uniform(50, 150))
+        self.col = Color(255, 0, 147)
 
     def show(self):
         fill(self.col)
@@ -22,12 +27,33 @@ class Spot:
         circle((self.i, self.j), 20, CENTER)
 
     def select(self):
-        fill(0)
+        fill(Color(20, 99, 255))
         no_stroke()
         circle((self.i, self.j), 20, CENTER)
 
     def __str__(self):
         return f'{self.i},{self.j}{self.index}'
+
+
+def textpos(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    mx, my = (x1+x2)/2, (y1+y2)/2
+    m = -1.0/((y2-y1)/(x2-x1))
+    ax, ay = 0, 0
+    l = 40
+    if m == 0:
+        ax = mx+l
+        ay = my
+    elif m > 10**9:
+        ax = mx
+        ay = my+l
+    else:
+        dx = 1/sqrt(1+(m*m))
+        dy = m*dx
+        ax = mx+dx
+        ay = my+dy
+    return ax, ay
 
 
 def findclosest():
@@ -45,24 +71,77 @@ def findclosest():
 
 def setup():
     size(width, height)
+    f = create_font("Arial.ttf", 18)
+    text_font(f)
 
 
 def draw():
+    global mode,dic,max_flow,msg
+    background(50)
+
+
+    if edge != None:
+        for i in range(len(edge)):
+            for j in range(len(edge)):
+                if edge[i][j] > 0 or (mode in {'r','d'} and edge[j][i] > 0):
+                    # TODO: edge is drawn here. change colors
+                    stroke(Color(255, 0, 147))
+                    stroke_weight(4)
+                    line((vertex[i].i, vertex[i].j),
+                         (vertex[j].i, vertex[j].j))
+                    x, y = textpos(
+                        (vertex[i].i, vertex[i].j), (vertex[j].i, vertex[j].j))
+                    fill(Color(255, 0, 147))
+                    no_stroke()
+                    if mode not in {'r','d'}:
+                        text(f'{edge[i][j]}', (x, y))
+                    else:
+                        if dic.get((i,j))!=None:
+                            print(i,j)
+                            text(f'{edge[j][i]}', (x, y))
 
     for spot in vertex:
         spot.show()
-    if mode=='e':
-        if start!=None:
+        if start != None:
+            stroke(Color(255, 0, 147))
+            stroke_weight(4)
+            line((start.i, start.j), (mouse_x, mouse_y))
             start.select()
-    if edge!=None:
+    
+    print(edge)
+    
+    if mode == 'r':
         for i in range(len(edge)):
-            for j in range(len(edge)):
+            for j in range(len(edge[0])):
                 if edge[i][j]>0:
-                    # TODO: edge is drawn here. change colors
-                    stroke(Color(0,255,153))
-                    stroke_weight(4)
-                    line((vertex[i].i,vertex[i].j),(vertex[j].i,vertex[j].j))
-    print(len(vertex))
+                    dic[(i,j)]=True
+        max_flow = solution(edge, src.index, sink.index)
+        print(max_flow)
+        msg="Done"
+        mode = 'd'
+        no_loop()
+    
+    if mode=='d':
+        fill(255)
+        no_stroke()
+        text(f'maximum flow:{max_flow}', (width-200, height-20))
+
+    fill(255)
+    no_stroke()
+    text(f'mode:{msg}', (10, height-20))
+    # if mode != 'r' or mode != 'd':
+
+    # for spot in vertex:
+    #     if mode == 'e':
+    #         if start != None and spot == start:
+    #             stroke(Color(255, 0, 147))
+    #             stroke_weight(4)
+    #             line((start.i, start.j), (mouse_x, mouse_y))
+    #             start.select()
+    #         else:
+    #             spot.show()
+    #     else:
+    #         spot.show()
 
 
 start = None
@@ -73,34 +152,65 @@ def mouse_pressed():
     global start, end
     if mode == 'e':
         start = findclosest()
-        
 
 
 def mouse_released():
-    global start, end, edge
+    global start, end, edge, mode
     if mode == 'e':
         end = findclosest()
-        edge[start.index][end.index]=1
+        # val=int(input())
+        try:
+            val = int(enterbox(msg="Edge capacity",
+                               title="Edge", default='', strip=True))
+        except ValueError:
+            val = 0
+        edge[start.index][end.index] = val
+        # edge[end.index][start.index] = val
         start, end = None, None
+        mode = 'e'
 
 
 def mouse_clicked():
+    global src, sink
     if mode == 'p':
         ind = len(vertex)
         vertex.append(Spot(mouse_x, mouse_y, ind))
+    elif mode == 's':
+        if src != None:
+            src.col = Color(255, 0, 147)
+        src = findclosest()
+        src.col = Color(0, 255, 0)
+    elif mode == 't':
+        if sink != None:
+            sink.col = Color(255, 0, 147)
+        sink = findclosest()
+        sink.col = Color(0, 0, 255)
 
 
 def key_pressed():
-    global mode, msg, edge
-    if mode != 'r':
+    global mode, msg, edge, weight, letters
+    if mode != 'r' and mode != 'd':
+        # if mode == 'i':
+        #     if key == "BACKSPACE":
+        #         if len(letters) > 0:
+        #             letters = letters[:-1]
+        #     elif key == "ENTER":
+        #         val = int(letters)
+        #         letters = ""
+        #         mode = 'e'
+        #         return val
+        #     else:
+        #         letters = letters + str(key)
+
         mode = str(key)
         if mode == 'p':
-            msg = "points"
+            msg = "Vertices"
         elif mode == 'e':
-            edge = [[0 for x in vertex] for y in vertex]
-            msg = "edges"
+            if edge == None:
+                edge = [[0 for x in vertex] for y in vertex]
+                msg = "Edges"
         elif mode == 's':
-            msg = "source"
+            msg = "Source"
         elif mode == 't':
             msg = "Target"
         elif mode == 'r':
